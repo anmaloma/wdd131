c2a977b78503119171ba89d8283c9d83283d3c4d
anmaloma <106412980+anmaloma@users.noreply.github.com>
2026-01-13 22:44:48 -0600

    Add files via upload

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Pone el año actual en el footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Muestra la fecha de última modificación
+document.getElementById("lastModified").textContent = document.lastModified;
