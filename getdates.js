a4c10632aff52fc03fa28395445d52698d647cc2
anmaloma <106412980+anmaloma@users.noreply.github.com>
2026-01-13 22:45:25 -0600

    Delete getdates.js

--- getdates.js
+++
@@ -1,5 +0,0 @@
-// Pone el año actual en el footer
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-// Muestra la fecha de última modificación
-document.getElementById("lastModified").textContent = document.lastModified;
