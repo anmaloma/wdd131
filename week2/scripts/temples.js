f8743c283d736c08f58884b1e2c2fb37b4978da3
anmaloma <106412980+anmaloma@users.noreply.github.com>
2026-01-19 20:09:11 -0600

    Delete week2 directory

--- week2/scripts/temples.js
+++
@@ -1,8 +0,0 @@
-const menuButton = document.getElementById('menu');
-const nav = document.getElementById('nav');
-
-menuButton.addEventListener('click', () => {
-    nav.classList.toggle('open');
-});
-
-document.getElementById("year")textcontent = new Date().getFullYear();
