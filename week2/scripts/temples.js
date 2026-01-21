86479b960093cf4fac0b5f760cddf58a0c672182
anmaloma <106412980+anmaloma@users.noreply.github.com>
2026-01-20 21:40:27 -0600

    Add files via upload

---
+++ week2/scripts/temples.js
@@ -0,0 +1,10 @@
+const menuButton = document.getElementById('menu');
+const nav = document.getElementById('nav');
+
+menuButton.addEventListener('click', () => {
+    nav.classList.toggle('open');
+});
+
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent =
+    " last Modified: " + document.lastModified;
